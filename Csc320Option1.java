06dbaa4315875b0248a276296df504a7d10e79c9
jhermosillo374 <jonathan.hermosillo@csuglobal.edu>
2024-08-17 07:49:03 -0600

    Add files via upload

---
+++ Csc320Option1.java
@@ -0,0 +1,18 @@
+public class Csc320Option1 {
+    public static void main(String[] args) {
+
+        String firstName = "Poe";
+        String lastName = "Hernandez";
+        String streetAddress = "234 Main St";
+        String city = "New York";
+        String zipCode = "32125";
+
+        System.out.println("First Name: " +firstName);
+        System.out.println("Last Name: " +lastName);
+        System.out.println("Street Adress: "+streetAddress);
+        System.out.println("City: "+city);
+        System.out.println("Zipcode: "+zipCode);
+
+    }
+   
+}
